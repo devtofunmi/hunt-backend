8252ff39c418a3bcfe36c77ea1cb76ccfac69d3b
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-04 00:19:31 +0100

    index

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import authRoutes from './route/auth.js'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Jay!')
+})
+
+
+app.use(
+  cors({
+    origin: (origin) => {
+      const allowedOrigins = [
+        'http://localhost:3000',
+        'https://launchhunt.up.railway.app',
+        'https://launchhunt.netlify.app',
+      ]
+      return allowedOrigins.includes(origin ?? '') ? origin : ''
+    },
+    credentials: true
+  })
+)
+
+app.route('/auth', authRoutes)
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
+
+
+
+
+export default app
+
