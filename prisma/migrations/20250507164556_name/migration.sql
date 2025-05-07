355b9ac6c69cf85f8e31757ba5c0334e731479a2
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-07 17:50:04 +0100

    ...

---
+++ prisma/migrations/20250507164556_name/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "image" TEXT,
+    "bio" TEXT,
+    "twitter" TEXT,
+    "github" TEXT,
+    "linkedin" TEXT,
+    "bluesky" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
