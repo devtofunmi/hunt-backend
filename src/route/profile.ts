d055d37746f3ac8b3c3dc77d5ba9d01848be9bba
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-08 14:04:54 +0100

    chnagemiddleware

--- src/route/profile.ts
+++ src/route/profile.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { getProfile, updateProfile } from '../controller/profile.js';
-import { authMiddleware } from '../middleware/authmiddleware.js';
+// import { authMiddleware } from '../middleware/authmiddleware.js';
+import { authMiddleware } from '../middleware/middleware.js';
 
 
 const profileRoutes = new Hono();
