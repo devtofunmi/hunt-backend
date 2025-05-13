d081a332fd9cd638964c6f47798df31c988b9216
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-05-13 15:07:45 +0100

    debug

--- src/route/profile.ts
+++ src/route/profile.ts
@@ -1,15 +1,20 @@
 import { Hono } from 'hono';
 import { getProfile, updateProfile } from '../controller/profile.js';
-// import { authMiddleware } from '../middleware/authmiddleware.js';
 import { authMiddleware } from '../middleware/middleware.js';
 
 
 const profileRoutes = new Hono();
 
-// Route to get the current user's profile
 profileRoutes.use('/', authMiddleware);
-profileRoutes.get('/', getProfile);
-profileRoutes.put('/', updateProfile);
+profileRoutes.get('/', (c) => {
+  console.log("GET /profile endpoint hit");
+  return getProfile(c);
+});
+profileRoutes.put('/', (c) => {
+  console.log("PUT /profile endpoint hit");
+  return updateProfile(c);
+});
+
 
 
 export default profileRoutes;
